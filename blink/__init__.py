e38dc9874f460275ad07cf3860a4c8a645be32b8
Stalin Sabu Thomas <44336699+legendof-selda@users.noreply.github.com>
2021-06-20 23:05:23 +0530

    added dumps&export, safe config reload and _update

--- blink/__init__.py
+++ blink/__init__.py
@@ -1,13 +1,27 @@
 import pathlib
-from .utils import load_config
+from .utils import load_config, dumps, export
 from typing import Union
+import traceback
 
 
 class Config(object):
     def __init__(self, config_path: Union[pathlib.Path, str], **kwargs):
         self.config_path = pathlib.Path(config_path)
-        self._modified = self.config_path.stat().st_mtime
-        self._config = load_config(self.config_path)
+        if not self.config_path.is_file():
+            if kwargs["create_file_if_not_exists"] is False:
+                self.config_path.touch()
+                self._config = {}
+            else:
+                raise FileNotFoundError("File does not exist!")
+        else:
+            self._config = load_config(self.config_path, **kwargs)
+        self._update()
+    
+    def dumps(self, type:str, **kwargs) -> str:
+        return dumps(self._config, type, **kwargs)
+
+    def export(self, path:Union[pathlib.Path, str], ext:str = None, **kwargs) -> str:
+        export(pathlib.Path(path), self._config, ext, **kwargs)
     
     def _check(self):
         if self.config_path.stat().st_mtime != self._modified:
@@ -15,8 +29,16 @@ class Config(object):
 
     def _reload(self):
         print(f"Reloading {self.config_path.name}")
+        try:
+            self._config = load_config(self.config_path)
+        except Exception as err:
+            print("Error Reloading config!")
+            traceback.print_tb(err.__traceback__)
+        finally:
+            self._update()
+
+    def _update(self):
         self._modified = self.config_path.stat().st_mtime
-        self._config = load_config(self.config_path)
 
     def __getitem__(self, key):
         self._check()
--- blink/utils.py
+++ blink/utils.py
@@ -4,34 +4,124 @@ import toml
 import pathlib
 from configparser import ConfigParser
 import xmltodict
+from typing import IO
 
 
-class InitDict(ConfigParser):
-    def to_dict(self):
+class ConfigParserExt(ConfigParser):
+    def to_dict(self) -> dict:
         d = dict(self._sections)
         for k in d:
             d[k] = dict(self._defaults, **d[k])
             d[k].pop("__name__", None)
         return d
+    
+    def dumps(self, space_around_delimiters=True) -> str:
+        """Return an .ini-format representation of the configuration state.
+
+        If `space_around_delimiters' is True (the default), delimiters
+        between keys and values are surrounded by spaces.
+        """
+        output = ""
+        if space_around_delimiters:
+            d = " {} ".format(self._delimiters[0])
+        else:
+            d = self._delimiters[0]
+        if self._defaults:
+            output += self._dumps_section(self.default_section,
+                                    self._defaults.items(), d)
+        for section in self._sections:
+            output += self._dumps_section(section,
+                                self._sections[section].items(), d)
+        return output
+
+    def _dumps_section(self, section_name, section_items, delimiter) -> str:
+        """Dump string a single section"""
+        output = "[{}]\n".format(section_name)
+        for key, value in section_items:
+            value = self._interpolation.before_write(self, section_name, key,
+                                                     value)
+            if value is not None or not self._allow_no_value:
+                value = delimiter + str(value).replace('\n', '\n\t')
+            else:
+                value = ""
+            output += "{}{}\n".format(key, value)
+        output += "\n"
+        return output
 
 
-def load_config(path: pathlib.Path, ext: str = None, **kwargs):
+def load_config(path: pathlib.Path, ext: str = None, **kwargs) -> dict:
     config: dict = None
     ext = path.suffix if ext is None else ext
     with path.open(mode="r") as config_file:
         if ext == ".json":
-            config = json.load(config_file)
+            config = json.load(config_file, **kwargs)
         elif ext in [".yml", ".yaml"]:
-            config = yaml.load(config_file, Loader=yaml.loader.SafeLoader)
+            if "Loader" not in kwargs.keys():
+                kwargs["Loader"] = yaml.loader.SafeLoader
+            config = yaml.load(config_file, **kwargs)
         elif ext == ".xml":
-            config = xmltodict.parse(config_file, **kwargs)
-        elif ext == ".ini":
-            ini_config = InitDict()
+            xml_input = config_file.read()
+            config = xmltodict.parse(xml_input, **kwargs)
+        elif ext in [".ini", ".cfg"]:
+            ini_config = ConfigParserExt()
             ini_config.read_file(config_file)
             config = ini_config.to_dict()
         elif ext == ".toml":
-            config = toml.load(config_file)
+            config = toml.load(config_file, **kwargs)
         else:
-            print("Currently supports ini, josn, toml, yaml, xml config files only")
-            raise Exception("File type not supported!")
+            print("Currently supports ini, json, toml, xml, yaml config files only")
+            raise ValueError("File type not supported!")
     return config
+
+
+def dumps(config: dict, ext: str, **kwargs):
+    config_str = ""
+    if ext == ".json":
+        if "indent" not in kwargs.keys():
+            kwargs["indent"] = 2
+        config_str = json.dumps(config, **kwargs)
+    elif ext in [".yml", ".yaml"]:
+        if "indent" not in kwargs.keys():
+            kwargs["indent"] = 2
+        config_str = yaml.safe_dump(config, **kwargs)
+    elif ext == ".xml":
+        if "pretty" not in kwargs.keys():
+            kwargs["pretty"] = True
+        if len(config.keys()) == 1:
+            config_str = xmltodict.unparse(config, **kwargs)
+        else:
+            config_str = xmltodict.unparse({"config": config}, **kwargs)
+    elif ext in [".ini", ".cfg"]:
+        ini_config = ConfigParserExt()
+        ini_config.read_dict(config, **kwargs)
+        config_str = ini_config.dumps()
+    elif ext == ".toml":
+        config_str = toml.dumps(config, **kwargs)
+    else:
+        print("Currently supports ini, json, toml, xml, yaml config files only")
+        raise ValueError("File ext not supported!")
+    return config_str
+
+
+def export(path: pathlib.Path, config: dict, ext: str = None, **kwargs):
+    ext = path.suffix if ext is None else ext
+    with path.open("w+") as config_file:
+        if ext == ".json":
+            json.dump(config, config_file, **kwargs)
+        elif ext in [".yml", ".yaml"]:
+            _ = yaml.safe_dump(config, config_file, **kwargs)
+        elif ext == ".xml":
+            if len(config.keys()) == 1:
+                config_str = xmltodict.unparse(config, **kwargs)
+            else:
+                config_str = xmltodict.unparse({"config": config}, **kwargs)
+            config_file.write(config_str)
+        elif ext in [".ini", ".cfg"]:
+            ini_config = ConfigParserExt()
+            ini_config.read_dict(config, **kwargs)
+            ini_config.write(config_file)
+        elif ext == ".toml":
+            config_str = toml.dump(config, config_file, **kwargs)
+        else:
+            print("Currently supports ini, json, toml, xml, yaml config files only")
+            raise ValueError("File ext not supported!")
